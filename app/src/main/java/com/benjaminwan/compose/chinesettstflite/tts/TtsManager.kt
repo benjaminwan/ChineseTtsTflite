376b8490f752861b8a624afbb7664641a4c3737f
benjaminwan <benjaminwan@163.com>
2021-08-30 11:02:14 +0800

    edit build.gradle, add Tensorflow Lite

---
+++ app/src/main/java/com/benjaminwan/compose/chinesettstflite/tts/TtsManager.kt
@@ -0,0 +1,10 @@
+package com.benjaminwan.compose.chinesettstflite.tts
+
+import android.content.Context
+
+object TtsManager {
+    fun initModels(context: Context) {
+        val fd = context.assets.openFd("")
+
+    }
+}
