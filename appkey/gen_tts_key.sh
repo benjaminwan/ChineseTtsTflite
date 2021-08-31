6a4a06a865fc9c7a085fd2699f95d6e5cdb5b0ce
benjaminwan <benjaminwan@163.com>
2021-08-31 08:12:03 +0800

    clean

--- appkey/gen_tts_key.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export ALIAS="chttstf"
-export FILENAME="$ALIAS.keystore"
-export PASS="chttstf"
-
-echo "gen android key"
-keytool -genkey -alias $ALIAS -storetype PKCS12 -keyalg RSA -validity 10000 -keystore $FILENAME -keypass $PASS -storepass $PASS -dname "CN=benjaminwan, OU=dev, O=work, L=Quanzhou, ST=Fujian, C=86"
